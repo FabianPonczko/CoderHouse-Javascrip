48d652e35c367f34270787142cbc1ab643c7c5e8
FabianPonczko <fabianponczko@live.com.ar>
2022-08-13 22:51:02 -0300

    Funcionando nuevo-borrar-modificasaldo-filtrar

--- javascriptDOM.js
+++ javascriptDOM.js
@@ -45,25 +45,13 @@ let clientesCargados = [
           this.numeroCuenta = numeroCuenta
           this.saldo = saldo
       }
-      //Sumar saldo de cliente
-      sumarSaldo (saldo){
-          this.saldo += saldo
-      }
-      //Restar saldo de cliente
-      restarSaldo (saldo){
-          if (this.saldo>=saldo){
-              this.saldo -= saldo
-          }else{
-              alert(`Saldo: ${this.saldo} imposible realizar la operacion`)
-          }
-      }
-      
+  
   }
   
-
+  //recuperar clientes del localstorage y sino carga los pre-cargados
+  const objetosRecuperados  = localStorage.getItem("storageClientes")
   
-  if (localStorage.getItem("storageClientes") != null){
-    const objetosRecuperados  = localStorage.getItem("storageClientes")
+  if (JSON.parse(objetosRecuperados)[0] !=undefined ){
     clientesCargados = JSON.parse(objetosRecuperados)
   }
   
@@ -75,155 +63,166 @@ let clientesCargados = [
       ele.numeroCuenta,
       ele.saldo,
   ))
-    
-    const tbody = document.getElementById("tbody")
-    const buscarClienteId = document.getElementById("buscarClienteId")
-    const cargadatos = document.getElementById("cargadatos")
-    const ocultarTabla  = document.getElementById("ocultarTabla")
-    const inputText = document.getElementById("inputText")
-    const selectId = document.getElementById("selectId")
-    const btnBorrar = document.getElementById("btnBorrar")
-    const btnVolver = document.getElementById("btnVolver")
-    let btnEliminar = false
-    let idValorElejido =``
+  
+  //Variables
+  const tbody = document.getElementById("tbody")
+  const buscarClienteId = document.getElementById("buscarClienteId")
+  const cargadatos = document.getElementById("cargadatos")
+  const ocultarTabla  = document.getElementById("ocultarTabla")
+  const inputText = document.getElementById("inputText")
+  const selectId = document.getElementById("selectId")
+  const btnBorrar = document.getElementById("btnBorrar")
+  const btnVolver = document.getElementById("btnVolver")
+  const inputSaldo = document.getElementById("inputSaldo")
+  const btnSalvar = document.getElementById("btnSalvar")
+  let btnEliminar = false
+  let idValorElejido =``
+  let idElejido =[]
     
 
     mostrarClientes()
    
 
+    //Input principal del nombre de cliente a buscar
     inputText.addEventListener("input",()=>{
         const encontrado = clientes.filter((ele=>ele.nombre.includes(inputText.value.toUpperCase())))
-        
         tbody.innerHTML = ``
         selectId.innerHTML = ``
         encontrado.forEach((ele)=>{
-            
             tbody.innerHTML += `
-            <tr>
-            <th id="fila${ele.id}" scope="row">${ele.id}</th>
-            <td>${ele.nombre}</td>
-            <td>${ele.direccion}</td>
-            <td>${ele.localidad}</td>
-            <td>${ele.numeroCuenta}</td>
-            <td>${ele.saldo}</td>
-            </tr>
-            `
-            selectId.innerHTML += `<option value="${ele.id}">${ele.id},${ele.nombre}</option>`
-            idElejido = encontrado
-            
+                                <tr>
+                                <th id="fila${ele.id}" scope="row">${ele.id}</th>
+                                <td>${ele.nombre}</td>
+                                <td>${ele.direccion}</td>
+                                <td>${ele.localidad}</td>
+                                <td>${ele.numeroCuenta}</td>
+                                <td>${ele.saldo}</td>
+                                </tr>
+                                `
+        selectId.innerHTML += `<option value="${ele.id}">${ele.id},${ele.nombre}</option>`
+        idElejido = encontrado
         })
-            idValorElejido = selectId.options[selectId.selectedIndex].value;
-            console.log("index del valor seleccionado: " + idValorElejido)
         
+        idValorElejido = selectId.options[selectId.selectedIndex].value;
      
+        //habilita selectid si encuentra clientes y muestra btnborrar si esta en funcion de borrar
         if (encontrado.length > 0){
             selectId.style.display=``
             if (btnEliminar){
                 btnBorrar.style.display=``
+                inputSaldo.style.display=`none`
             }   
         }else{
             selectId.style.display=`none`
             btnBorrar.style.display=`none`
+            inputSaldo.style.display=`none`
         }
     })
 
-    selectId.addEventListener("change",()=>{
-                   
-        tbody.innerHTML =``
-        tbody.innerHTML = `
-        <tr>
-        <th id="fila${idElejido[selectId.selectedIndex].id}" scope="row">${idElejido[selectId.selectedIndex].id}</th>
-        <td>${idElejido[selectId.selectedIndex].nombre}</td>
-        <td>${idElejido[selectId.selectedIndex].direccion}</td>
-        <td>${idElejido[selectId.selectedIndex].localidad}</td>
-        <td>${idElejido[selectId.selectedIndex].numeroCuenta}</td>
-        <td>${idElejido[selectId.selectedIndex].saldo}</td>
-        </tr>
-        `
+        //select desplegable
+        selectId.addEventListener("change",()=>{          
+            tbody.innerHTML =``
+            tbody.innerHTML = `
+                            <tr>
+                            <th id="fila${idElejido[selectId.selectedIndex].id}" scope="row">${idElejido[selectId.selectedIndex].id}</th>
+                            <td>${idElejido[selectId.selectedIndex].nombre}</td>
+                            <td>${idElejido[selectId.selectedIndex].direccion}</td>
+                            <td>${idElejido[selectId.selectedIndex].localidad}</td>
+                            <td>${idElejido[selectId.selectedIndex].numeroCuenta}</td>
+                            <td>${idElejido[selectId.selectedIndex].saldo}</td>
+                            </tr>
+                            `
             idValorElejido = selectId.options[selectId.selectedIndex].value;
-            console.log("index del valor seleccionado: " + idValorElejido)
-    })
+            
+        })
 
+    //boton volver sin cambios
     btnVolver.addEventListener("click",()=>{
         mostrarClientes()
     })
 
+    
+    //boton borrar cliente y actualiza localstorage
+    btnBorrar.addEventListener("click",()=>{
+       const indice = clientes.findIndex((ele)=>ele.id == idValorElejido)
+       clientes.splice(indice,1)
+       localStorage.setItem('storageClientes', JSON.stringify(clientes));
+       mostrarClientes()
+    })
+
+    
+    //muestra lista de clientes
     function mostrarClientes(){
         buscarClienteId.style.display=`none`
         cargadatos.style.display=`none`
         btnBorrar.style.display=`none`
-        //btnVolver.style.display=`none`
         ocultarTabla.style.display = ``
-        
-        tbody.innerHTML = `
-            <tr>
-            <th scope="row"></th>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            </tr>
-            `
-        clientes.forEach((info)=>{
-           tbody.innerHTML += `
-           <tr>
-           <th scope="row">${info.id}</th>
-           <td>${info.nombre}</td>
-           <td>${info.direccion}</td>
-           <td>${info.localidad}</td>
-           <td>${info.numeroCuenta}</td>
-           <td>${info.saldo}</td>
-           </tr>
-           `
+        tbody.innerHTML =   ` <tr>
+                            <th scope="row"></th>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            </tr> `
+
+        clientes.forEach((info)=>{   
+            tbody.innerHTML += `<tr>
+                                <th scope="row">${info.id}</th>
+                                <td>${info.nombre}</td>
+                                <td>${info.direccion}</td>
+                                <td>${info.localidad}</td>
+                                <td>${info.numeroCuenta}</td>
+                                <td>${info.saldo}</td>
+                                </tr>`
         })
       
     }
     
-    function nuevoCliente(){
-        
+    //Crea formulario para nuevo cliente
+    nuevoCliente.addEventListener("click",()=>{
         ocultarTabla.style.display=`none`        
-        
         cargadatos.style.display=``        
-        
-        cargadatos.innerHTML = (` 
-        <div class="mt-4 mb-3 ms-3">
-        <label for="formGroupExampleInput" class="form-label">Nombre</label>
-        <input type="text" class="form-control" id="nombre" placeholder="ingrese nombre">
-        </div>
-        <div class="mb-3 ms-3">
-        <label for="formGroupExampleInput2" class="form-label">Direccion</label>
-            <input type="text" class="form-control" id="direccion" placeholder="ingrese dirección">
-            </div>
-            <div class="mb-3 ms-3">
-            <label for="formGroupExampleInput3" class="form-label">Localidad</label>
-            <input type="text" class="form-control" id="localidad" placeholder="ingrese localidad">
-            </div>
-            <div class="mb-3 ms-3">
-            <label for="formGroupExampleInput5" class="form-label">saldo</label>
-            <input type="text" class="form-control" id="saldo" placeholder="ingrese localidad">
-            </div>
-            <button type="submit" class="btn btn-primary ms-3" onclick="cargaDatos()">Submit</button>
-            <button type="cancel" class="btn btn-primary ms-3" onclick="mostrarClientes()">Cancelar</button>
+        cargadatos.innerHTML = `<div class="mt-4 mb-3 ms-3">
+                                <label for="formGroupExampleInput" class="form-label">Nombre</label>
+                                <input type="text" class="form-control" id="nombre" placeholder="ingrese nombre">
+                                </div>
+                                <div class="mb-3 ms-3">
+                                <label for="formGroupExampleInput2" class="form-label">Direccion</label>
+                                <input type="text" class="form-control" id="direccion" placeholder="ingrese dirección">
+                                </div>
+                                <div class="mb-3 ms-3">
+                                <label for="formGroupExampleInput3" class="form-label">Localidad</label>
+                                <input type="text" class="form-control" id="localidad" placeholder="ingrese localidad">
+                                </div>
+                                <div class="mb-3 ms-3">
+                                <label for="formGroupExampleInput5" class="form-label">saldo</label>
+                                <input type="text" class="form-control" id="saldo" placeholder="ingrese localidad">
+                                </div>
+                                <button type="submit" class="btn btn-primary ms-3" onclick="cargaDatos()">Submit</button>
+                                <button type="cancel" class="btn btn-primary ms-3" onclick="mostrarClientes()">Cancelar</button>`
+    })
 
-        `)
-    }
-
+    
+    //Crea nuevo cliente y guarda en storage
     function cargaDatos(){
-        
         const nombre = document.getElementById("nombre")
         const direccion = document.getElementById("direccion")
         const localidad = document.getElementById("localidad")
         const saldo = document.getElementById("saldo")
-       
+    
+        //Busco el id maximo y le sumo 1 para el nuevo cliente
         const idMax = clientes.reduce (function(acc,ele){
             if (acc.id < ele.id){
                 return acc.id + 1
             }else{
                 return ele.id + 1
             }
-        })
+        },1)
+    
+        //Crea numero aleatorio para numero de cuenta
         let numeroAleatorio = Math.round(Math.random()*100000);
+    
         const nuevoCliente = new Cliente (idMax ,nombre.value.toUpperCase(),direccion.value,localidad.value.toUpperCase(),numeroAleatorio,saldo.value)
         clientes.push(nuevoCliente)
         buscarClienteId.style.display=`none`
@@ -232,54 +231,56 @@ let clientesCargados = [
     }
     
   
-    let idElejido =[]
-    function clienteAeliminar(){
+    
+    //Actualiza la paguina con botones para eliminar el cliente
+    clienteAeliminar.addEventListener("click",()=>{
         btnEliminar = true
+        inputSaldo.style.display=`none`
         btnVolver.style.display=`none`
+        btnSalvar.style.display=`none`
         mostrarClientes()
         buscarClienteId.style.display=``
         selectId.style.display=`none`
         inputText.value=``
         
             
-            //const aBorrar = idElejido[selectId.selectedIndex]
-            //console.log(`idElejido= ${idElejido}`)
-
-            
-                btnBorrar.addEventListener("click",()=>{
-                    //clientes.splice(idElejido[selectId.selectedIndex],1)
-                    //localStorage.setItem('storageClientes', JSON.stringify(clientes));
-                   // clientes.splice(idElejido[selectId.selectedIndex],1)
-                   console.log(clientes + `clientes`)
-                   console.log(idElejido[selectId.selectedIndex].id)
-                   const indice = clientes.findIndex((ele)=>ele.id == idValorElejido)
-                   console.log("indice de cliente a borrar: " + indice)
-                   console.log(clientes[indice])
-                   //clientes.splice(indice,1)
-                   //localStorage.setItem('storageClientes', JSON.stringify(clientes));
-                   mostrarClientes()
-                })
-        /*
-        for (let ele = 0; ele<clientes.length ; ele++){
-                    if (clientes[ele].nombre == eliminar){
-                    alert(`se elimino cliente: ${clientes[ele].nombre}`)
-                    clientes.splice(ele,1)
-                    }
-                } 
-                localStorage.setItem('storageClientes', JSON.stringify(clientes));
-                mostrarClientes()
-                */
-    }
-    function filtarCliente(){
-        
+          
+    })
+    
+    //Prepara paguina para filtrado de cliente por nombre
+    filtarCliente.addEventListener("click",()=>{
         btnEliminar = false
+        inputSaldo.style.display=`none`
         mostrarClientes()
         buscarClienteId.style.display=``
         selectId.style.display=`none`
         btnBorrar.style.display=`none`
+        btnSalvar.style.display=`none`
         btnVolver.style.display=``
         inputText.value=``
         
-    }
+    })
 
+    //Prepara paguina con botones para modificar saldo saldo 
+    ingresoSaldo.addEventListener("click",()=>{
+        btnEliminar = false
+        mostrarClientes()
+        inputText.value=""
+        inputSaldo.value=""
+        selectId.style.display=`none`
+        btnBorrar.style.display=`none`
+        btnVolver.style.display=`none`
+        buscarClienteId.style.display=``
+        inputSaldo.style.display=``
+        btnSalvar.style.display=``
+    })
     
+    //Modifica saldo
+    btnSalvar.addEventListener("click",()=>{
+       const indice = clientes.findIndex((ele)=>ele.id == idValorElejido)
+       const saldo = parseFloat(clientes[indice].saldo)
+       const nuevosaldo = parseFloat(inputSaldo.value) + saldo
+       clientes[indice].saldo = nuevosaldo
+       localStorage.setItem('storageClientes', JSON.stringify(clientes));
+       mostrarClientes()
+    })
